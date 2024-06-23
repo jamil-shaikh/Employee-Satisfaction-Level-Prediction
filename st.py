10a0323a013a19f12b5ef995f562a33804f60331
jamil-shaikh <69137290+jamil-shaikh@users.noreply.github.com>
2024-06-23 14:41:11 -0400

    Update st.py

--- st.py
+++ st.py
@@ -3,17 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-import os
-
-
-
-
-# Verify the file path
-file_path = 'HR_comma_sep.csv'
-if not os.path.isfile(HR):
-    raise FileNotFoundError(f"The file {HR_comma_sep.csv} does not exist.")
-
-
 
 # Load the model
 model_2 = load('model_2.pkl')
